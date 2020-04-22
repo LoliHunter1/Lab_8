e8b8734638866d3c9fbb7ccfc10d6efdbab62419
LoliHunter1 <47854086+LoliHunter1@users.noreply.github.com>
2020-04-22 14:17:16 +0300

    Add files via upload

---
+++ зад4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace зад4
+{
+    class Program
+    {
+        static void Main()
+        {
+            string a = "Строка и несколько слов";
+            string[] b = a.Split();
+            Array.Sort(b);
+            a = String.Join(' ', b);
+            Console.WriteLine(a);
+        }
+    }
+}

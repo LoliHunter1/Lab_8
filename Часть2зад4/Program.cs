f69884b708905c1373ef0d92b1fb9365774eaefa
LoliHunter1 <47854086+LoliHunter1@users.noreply.github.com>
2020-04-22 14:26:03 +0300

    Add files via upload

---
+++ Часть2зад4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Часть2зад4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Int16 a = 3;
+            Int32 b = 4;
+            double c = 5;
+            c = a;
+            c = b;
+            // a = c;
+            // a = b;
+            b = a;
+            // b = c;
+            a = (Int16)b;
+            a = (Int16)c;
+            Console.WriteLine(a);
+        }
+    }
+}
